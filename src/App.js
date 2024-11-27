6af06fda2e664ccaba11434fb64be791a86fbe1f
yoom <fdgh134@naver.com>
2024-11-27 22:51:20 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  
+  const clickPop = () => {
+    alert("클릭!");
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      textAlign: "center",
+     }}
+    >
+    <p>아래 버튼을 클릭해봐</p>
+    <button onClick={clickPop}>여기</button>
     </div>
   );
 }
